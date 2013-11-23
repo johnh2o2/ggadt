Produces dQscat/dOmega with the correct shape, however there appears to be a missing factor of 2pi or so...(Magnitude is ~9 times lower than it should be)

plot.py
@@ -2,6 +2,8 @@ from math import *
 import numpy as np 
 import matplotlib.pyplot as plt
 
+
+
 fname = 'testdat.dat'
 data_dt = np.dtype([('theta', np.float_), ('dscatdomega_x', np.float_), ('dscatdomega_y', np.float_)])
 data = np.loadtxt(fname,dtype=data_dt)
@@ -14,9 +16,24 @@ ax.plot(conv*data['theta'],data['dscatdomega_x'], label="x-axis", color='b', ls=
 ax.plot(conv*data['theta'], data['dscatdomega_y'], label="y-axis", color='r', ls='--')
 ax.set_ylabel("$d\\sigma_{scat.}/d\\Omega$")
 ax.set_yscale('log')
-ax.set_ylim(0.1,100000)
-ax.set_xlim(0,5000)
+ax.set_ylim(0.1,200000)
+ax.set_xlim(0,4000)
 #ax.set_xscale('log')
 ax.set_xlabel("$\\theta$ [radians]")
 ax.legend(loc='best')
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+