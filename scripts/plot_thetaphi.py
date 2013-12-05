Does orientation averaging. Rotation matrix should be changed to avoid doing the product of three matrices. ROT_MATRIX does not give correct results, must be double checked. Tested with ellipsoids.

scripts/plot_thetaphi.py
@@ -1,7 +1,7 @@
 from math import *
 import sys
 import numpy as np 
-from scipy.interpolate import interp2d
+from scipy.interpolate import RectBivariateSpline
 import matplotlib.pyplot as plt
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
@@ -15,19 +15,35 @@ x = data['theta']*conv
 y = data['phi']*conv
 z = data['f']
 
-delta = 0.001
-xmin = -2000 - delta
-xmax = 2000 + delta 
+ALL_ZERO = True 
+
+for Z in z:
+	if Z != 0:
+		#print "Not all zero :)"
+		ALL_ZERO = False
+		break
+
+if ALL_ZERO:
+	print "ALL ZERO!"
+	sys.exit()
+
+dx = x[1] - x[0]
+dy = y[1] - y[0]
+
+print "dx = %e; dy = %e"%(dx,dy)
+delta = 500
+xmin = -2000
+xmax = 2000
 ymin = xmin
 ymax = xmax
-
+'''
 def smooth(x_old,y_old,z_old,old_dim,new_dim):
 	print "smoothing from ",old_dim," to ",new_dim
 	if old_dim <= new_dim : return x_old,y_old,z_old
 	x_new = np.zeros(new_dim*new_dim)
 	y_new = np.zeros(new_dim*new_dim)
 	z_new = np.zeros(new_dim*new_dim)
-	num = int(float(old_dim)/float(new_dim))
+	
 	print old_dim,"=",num,"*",new_dim
 	for i in range(0,new_dim):
 		for j in range(0,new_dim):
@@ -42,13 +58,14 @@ def smooth(x_old,y_old,z_old,old_dim,new_dim):
 	print "Done!"
 
 	return x_new,y_new,z_new
-'''				
+'''
+
 x_new = []
 y_new = []
 z_new = []
 
 for i in range(0,len(x)):
-	if x[i] >= xmin and x[i] <= xmax and y[i] >= ymin and y[i] <= ymax:
+	if x[i] >= (xmin-delta) and x[i] <= (xmax+delta) and y[i] >= (ymin-delta) and y[i] <= (ymax+delta):
 		x_new.append(x[i])
 		y_new.append(y[i])
 		z_new.append(z[i])
@@ -56,50 +73,43 @@ for i in range(0,len(x)):
 x = np.array(x_new)
 y = np.array(y_new)
 z = np.array(z_new)
-'''
-print sqrt(len(x))
+print sqrt(len(x)), sqrt(len(y)), sqrt(len(z))
 
-ALL_ZERO = True 
 
-for Z in z:
-	if Z != 0:
-		#print "Not all zero :)"
-		ALL_ZERO = False
-		break
 
-if ALL_ZERO:
-	print "ALL ZERO!"
-	sys.exit()
+n = 0
 
-nx = 0
-ny = 0
+while x[n] == x[0]: n+=1
 
-while x[ny] == x[0]: ny+=1
 
-nx = len(x)/ny
+print int(sqrt(len(x))), n
+#print x, y, z 
 
-dx = (x.max() - x.min())/(nx-1)
-dy = (y.max() - y.min())/(ny-1)
-'''
-grid_size = 64
-x,y,z = smooth(x,y,z,nx,grid_size)
-ny = 0
-while x[ny] == x[0]: ny+=1
-nx = len(x)/ny
-print int(sqrt(len(x))), ny, nx 
-print x, y, z 
-'''
-print "Going to interpolate..."
-zinterp = interp2d(x,y,np.log(z))
-print "Interpolated."
 
-x = np.reshape(x,(nx,nx))
-y = np.reshape(y,(nx,nx))
-z = np.reshape(z,(nx,nx))
-print x,y,z 
-zcont = np.log10(z)
+x = np.reshape(x,(n,n))
+y = np.reshape(y,(n,n))
+z = np.reshape(z,(n,n))
 
-levels = MaxNLocator(nbins=20).tick_values(zcont.min(), zcont.max())
+
+print "--Going to interpolate..."
+BSPL = RectBivariateSpline(x[:,0],y[0,:],np.log10(z))
+zinterp = lambda xi,yi : BSPL.ev(xi,yi)
+print "  +Interpolated."
+
+
+
+nplot = 500
+zplot = np.zeros((nplot,nplot))
+xplot,yplot = np.meshgrid(np.linspace(xmin,xmax,nplot),np.linspace(ymin,ymax,nplot))
+for i in range(0,nplot):
+	for j in range(0,nplot):
+		zplot[i][j] = zinterp(xplot[i][j],yplot[i][j])
+
+#xplot = x
+#yplot = y
+#zplot = np.log10(z)
+
+levels = MaxNLocator(nbins=40).tick_values(zplot.min(), zplot.max())
 
 # pick the desired colormap, sensible levels, and define a normalization
 # instance which takes data values and translates those into levels.
@@ -108,17 +118,19 @@ norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
 
 f = plt.figure(1)
 ax = f.add_subplot(111)
-colorplot = ax.pcolormesh(x,y,zcont,cmap=cmap, norm=norm)
+colorplot = ax.pcolormesh(xplot,yplot,zplot,cmap=cmap, norm=norm)
 #plt.contourf(x[:-1, :-1] + dx / 2.,y[:-1, :-1] + dy / 2.,z,levels=levels, cmap=cmap)
 ax.set_ylabel("$\\theta_Y$ [arcseconds]")
 ax.set_xlabel("$\\theta_X$ [arcseconds]")
-ax.axis([xmin,xmax,ymin,ymax])
+ax.axis([xplot.min(),xplot.max(),xplot.min(),xplot.max()])
 plt.colorbar(colorplot)
 
-print "Plotted figure 1"
+print "--Plotted figure 1"
+#plt.show()
+#sys.exit()
 f2 = plt.figure(2)
 ax2 = f2.add_subplot(111)
-theta_arr = np.linspace(xmin,xmax,100)
+theta_arr = np.linspace(xmin,xmax,1000)
 
 
 z_phiavg = np.zeros(len(theta_arr))
@@ -126,34 +138,26 @@ z_phivar = np.zeros(len(theta_arr))
 z_max = np.zeros(len(theta_arr))
 z_min = np.zeros(len(theta_arr))
 phi_ints = np.linspace(0,2*np.pi,100)
-print "Initialized other arrays"
+
 
 for i,phi in enumerate(phi_ints):
-	
 	for j, th in enumerate(theta_arr):
 		z_phi = pow(10,zinterp(th*cos(phi),th*sin(phi)))
 		if i==0:
 			z_max[j] = z_phi
 			z_min[j] = z_phi
+
 		if z_max[j] < z_phi: z_max[j] = z_phi
 		if z_min[j] > z_phi: z_min[j] = z_phi
+
 		z_phiavg[j]+=z_phi/len(phi_ints)
 for i,phi in enumerate(phi_ints):
 	for j, th in enumerate(theta_arr):
 		z_phi = pow(10,zinterp(th*cos(phi),th*sin(phi)))
 		z_phivar[j]+=pow(z_phi-z_phiavg[j],2)/len(phi_ints)
 
-print "Calculated averages and stuff"
 
 z_phivar = np.power(z_phivar,0.5)
-print "Calculated z_phivar"
-#print "old sigma = ",sum(z_phivar)/len(z_phivar)
-#z_phiavg = np.array([ pow(10,z_phiavg[i]) for i in range(0,len(theta_arr))])
-#z_phivar = np.array([ pow(10,z_phivar[i]) for i in range(0,len(theta_arr))])
-#z_max = np.array([ pow(10,z_max[i]) for i in range(0,len(theta_arr))])
-#z_min = np.array([ pow(10,z_min[i]) for i in range(0,len(theta_arr))])
-
-#print "sigma = ",sum(z_phivar)/len(z_phivar)
 
 phis = [ 0.0, np.pi/2.0 ]
 colors = [ 'b', 'r', 'c', 'g' ]