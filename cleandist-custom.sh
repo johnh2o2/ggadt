757b0b3010d242df66eb07e972da434e24174869
John Hoffman <jah5@princeton.edu>
2014-01-10 21:42:39 -0600

    Working version of autoconf

---
+++ cleandist-custom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -f aclocal.m4
+rm -r -f autom4te.cache/
+rm  -f *\.o
+rm  -f *\.mod
+rm -f config.status
+rm  -f config.log
+rm  -f configure
+rm  -f Makefile
+rm -f install-sh
+rm -f missing
+rm -f src/Makefile
+rm -f src/Makefile.in
+rm -f src/ggadt
+rm -f src/*o
+rm -f src/*mod
