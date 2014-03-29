af607588a85befd123fd334b9c73a0ae1dacce6d
John Hoffman <jah5@princeton.edu>
2014-03-29 13:55:24 -0400

    Fixed errors in principal axis rotation, updated documentation to reflect changes and included figures

--- test-axis-avg.sh
+++ test-axis-avg.sh
@@ -3,10 +3,18 @@
 bash reconf-custom.sh || exit
 ./configure --enable-openmp || exit
 make || exit
-
+<<COMMENT
 echo "Sequential orientations"
 src/ggadt --grain-axis-x=0.5 --grain-axis-z=0.5 --grain-geometry=ellipsoid --spin-axis=xaxis --euler-angle-mode=sequential > output.dat || exit
 python scripts/plot.py output.dat
+COMMENT
 echo "Random orientations"
-src/ggadt --grain-axis-x=0.5 --grain-axis-z=0.5 --grain-geometry=ellipsoid --spin-axis=xaxis --euler-angle-mode=random > output.dat || exit
-python scripts/plot.py output.dat
+echo "X-axis:"
+src/ggadt --grain-axis-y=0.5 --grain-axis-z=0.5 --grain-geometry=ellipsoid --rotation-axis=xaxis --euler-angle-mode=random > outputx.dat || exit
+python scripts/plot.py outputx.dat &
+echo "y-axis:"
+src/ggadt --grain-axis-y=0.5 --grain-axis-z=0.5 --grain-geometry=ellipsoid --rotation-axis=yaxis --euler-angle-mode=random > outputy.dat || exit
+python scripts/plot.py outputy.dat &
+echo "z-axis:"
+src/ggadt --grain-axis-y=0.5 --grain-axis-z=0.5 --grain-geometry=ellipsoid --rotation-axis=zaxis --euler-angle-mode=random > outputz.dat || exit
+python scripts/plot.py outputz.dat
