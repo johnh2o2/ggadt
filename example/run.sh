135ffeaebab4d52376241a1afa7e78e1111526db
John Hoffman <jah5@princeton.edu>
2015-09-14 17:58:13 -0400

    Centered spheres now (this only matters if you output the shadow function), and updated the documentation. The example folder has also now been updated to reflect the addition of the 2d case

--- example/run.sh
+++ example/run.sh
@@ -2,8 +2,10 @@ ggadt_binary=../src/ggadt
 
 $ggadt_binary --parameter-file=parameters_diffscat.ini > diffscat.dat || exit 1
 $ggadt_binary --parameter-file=parameters_total_xs.ini > total_xs.dat || exit 1
+$ggadt_binary --parameter-file=parameters_diffscat_2d.ini > diffscat_2d.dat || exit 1
 
 python plot_diffscat.py diffscat.dat &
+python plot_diffscat_2d.py diffscat_2d.dat &
 python plot_total_xs.py total_xs.dat &
 
 echo "Plots should show up; they take a moment."
